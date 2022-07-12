c2d13518287915963df7d66604750661b2ea0ceb
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-07-12 17:20:56 -0300

    list blog posts

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
+
+  // Copy all PNG images directly to build
+  eleventyConfig.addPassthroughCopy("**/*.png");
 };
