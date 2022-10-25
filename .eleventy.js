173357ffb602cbb6cf5b7e3afb3001063b7e760c
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-10-25 16:27:05 -0300

    add images to homepage

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
 
-  // Copy all PNG images directly to build
-  eleventyConfig.addPassthroughCopy("src/**/*.png");
-
   return {
     dir: {
       input: "src",
