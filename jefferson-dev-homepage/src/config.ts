35e9044ec576d6455d33041ddcd02bdd1dcab3a8
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2022-12-17 21:56:44 -0300

    create header

---
+++ jefferson-dev-homepage/src/config.ts
@@ -0,0 +1,27 @@
+export const CONTACT_INFO = [
+  {
+    name: "Github",
+    link: "https://github.com/jeffersonataide",
+    icon: "/github.svg",
+  },
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/jefferson-ataide-oliveira/?locale=en_US",
+    icon: "/linkedin.svg",
+  },
+  {
+    name: "Twitter",
+    link: "https://twitter.com/jeff_dev_",
+    icon: "/twitter.svg",
+  },
+  {
+    name: "Email",
+    link: "mailto:jefferson.dev.insights@gmail.com",
+    icon: "/mail.svg",
+  },
+];
+
+export const ROUTES = [
+  { name: "Home", link: "/" },
+  { name: "Blog", link: "/blog" },
+];
