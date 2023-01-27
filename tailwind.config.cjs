2b9033e5cc4050e653eba399d0c20508ec06ff85
Jefferson Oliveira <jefferson.dev.insights@gmail.com>
2023-01-27 15:54:34 -0300

    create theme colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.violet['200'],
+        secondary: colors.violet['600']
+      },
+    },
+  },
+  plugins: [],
+};
